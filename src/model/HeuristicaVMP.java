1197138711427985d29cb2982399e41a3a1af21a
Ednaldo <martinsednaldo@outlook.com>
2018-10-24 11:34:51 -0300

    Heurística do vizinho mais proximo

---
+++ src/model/HeuristicaVMP.java
@@ -0,0 +1,21 @@
+
+package model;
+
+import util.Heuristica;
+
+/*******************************************************************************
+ * @author Ednaldo                                                             *
+ *  date 24.10.2018                                                            *
+ ******************************************************************************/
+public class HeuristicaVMP implements Heuristica
+{
+    /*
+    VMP: Vizinho mais proximo
+    SCV: Solucao do caixeiro viajante
+    */
+    @Override
+    public void solucionarPCV(Rota[][] grafo, Rota[][] grafoSCV)
+    {
+        
+    }
+}
