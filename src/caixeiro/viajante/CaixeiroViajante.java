98a59e8512b9c73c954d91f03f3a3ef040e5eec1
Ednaldo <martinsednaldo@outlook.com>
2018-10-25 15:05:22 -0300

    Testes de execução

--- src/caixeiro/viajante/CaixeiroViajante.java
+++ src/caixeiro/viajante/CaixeiroViajante.java
@@ -1,6 +1,10 @@
 
 package caixeiro.viajante;
 
+import controller.ControllerPCV;
+import model.HeuristicaVMP;
+
+
 /*******************************************************************************
  * @author Ednaldo                                                             *
  *  date: 23.10.2018                                                           *
@@ -9,8 +13,14 @@ public class CaixeiroViajante {
 
     public static void main(String[] args)
     {
-        
-        
+        //intancia da classe controladora
+        ControllerPCV control = new ControllerPCV();
+        //print da matriz de origem
+        control.printMatrizPCV();
+        //geracao da solucao para o PCV com base na heuristica usada
+        control.controlePCV( new HeuristicaVMP() );
+        //print do calculo
+        control.printResultadoSCV();
     }
     
 }
