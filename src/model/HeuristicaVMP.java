9fbccdd0be6f6df5119ea06c636d42a089ff8893
Ednaldo <martinsednaldo@outlook.com>
2018-10-26 15:01:52 -0300

    Solução PCV para matriz do tipo long.

--- src/model/HeuristicaVMP.java
+++ src/model/HeuristicaVMP.java
@@ -11,10 +11,57 @@ import util.Heuristica;
 public class HeuristicaVMP implements Heuristica
 {
     int contRotas[];
+    int inicio;
     /*
     VMP: Vizinho mais proximo
     SCV: Solucao do caixeiro viajante
     */
+    //nova funcao simplificada
+    @Override
+    public void solucionarPCV(long[][] grafo, int[] vetorSCV)
+    {
+        //contador de rotas por cidade
+        contRotas = new int[grafo.length];
+        //ponto de partida escolhido
+        int inicio = 0, cidadeAtual = inicio, proximaCidade = -1, n = 0;
+        vetorSCV[0] = inicio;
+        contRotas[inicio]++;
+        while(!isHamiltoniano(vetorSCV)) 
+        {
+            long menor = Long.MAX_VALUE;
+            proximaCidade = -1;
+            for(int j = 0; j < grafo.length; j++)
+            {
+                if( grafo[cidadeAtual][j]!= 0 && grafo[cidadeAtual][j] < menor 
+                    && contRotas[j] < 2 && j != inicio)
+                {
+                    proximaCidade = j;
+                    menor = grafo[cidadeAtual][proximaCidade];
+                }
+            }
+            
+            if(proximaCidade != -1)
+            {   //teoricamente encontrou uma proxima rota
+                addCidade(grafo, vetorSCV, cidadeAtual, proximaCidade, ++n);
+                cidadeAtual = proximaCidade;
+            }
+        }
+    }
+    
+    private void addCidade( long [][] grafo, int [] vetor, int i, int j, int pos)
+    {
+        vetor[pos] = j;
+        contRotas[i]++;
+        contRotas[j]++;
+        vetor[pos+1] = inicio;
+    }
+    
+    //contador simplificado usando vetor
+    private boolean isHamiltoniano( int [] vetor)
+    {
+        return !(vetor[vetor.length-1] != vetor[0]);
+    }
+    
     @Override
     public void solucionarPCV(Rota[][] grafo, Rota[] vetorSCV)
     {
@@ -23,7 +70,7 @@ public class HeuristicaVMP implements Heuristica
         FuncoesVetor.iniciarVetor(contRotas);
         //ponto de partida escolhido
         int inicio = 0;
-        int i = inicio, posicao = 0;
+        int i = inicio, n = 0;
         contRotas[inicio]++;
         while(!isHamiltoniano(vetorSCV))
         {
@@ -46,8 +93,8 @@ public class HeuristicaVMP implements Heuristica
                 grafo[vmp.getCidade1()][vmp.getCidade2()].setSelecionada(true);
                 grafo[vmp.getCidade2()][vmp.getCidade1()].setSelecionada(true);
                 vmp.setSelecionada(true);
-                addRota(grafo, vetorSCV, vmp, posicao);
-                posicao++;
+                addRota(grafo, vetorSCV, vmp, n);
+                n++;
                 i = vmp.getCidade2();
             }
          
