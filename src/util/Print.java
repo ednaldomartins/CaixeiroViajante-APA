c51db183b913c1395a6efa66009ade79c419c26a
Ednaldo <martinsednaldo@outlook.com>
2018-10-26 14:59:39 -0300

    Prints para matriz long.

--- src/util/Print.java
+++ src/util/Print.java
@@ -20,18 +20,15 @@ public class Print
         }
     }
     
-    public static void printResultado (Rota [][] matrizSCV)
+    public static void printMatriz (long[][] matriz)
     {
-        if(matrizSCV != null) {
-            long resultadoPCV = 0;
-            for(int i = 0; i < matrizSCV.length; i++)
-                for(int j = 0; j < matrizSCV.length; j++)
-                    resultadoPCV += matrizSCV[i][j].getDistancia();
-            System.out.println("SCV. Tamanho: " + matrizSCV.length);
-            System.out.println("SCV. Resultado: " + resultadoPCV);
+        System.out.println("\n\nMatriz. Tamanho: " + matriz.length );
+        for(int i = 0; i < matriz.length; i++)
+        {
+            for(int j = 0; j < matriz.length; j++)
+                System.out.print( " " + matriz[i][j]);
+            System.out.println();
         }
-        else
-            System.out.println("Matriz vazia");
     }
     
     public static void printVetor (Rota [] vetor)
@@ -44,6 +41,14 @@ public class Print
                                 "______" + vetor[i].getDistancia()+ "______" + 
                                 "(" + vetor[i].getCidade2()+ ")" );
         }
+    }
+    
+    public static void printVetor (int [] vetor)
+    {
+        System.out.println("\n\nSCV. Tamanho: " + vetor.length + "\n[ ");
+        for(int i = 0; i < vetor.length; i++)
+            System.out.println( vetor[i] + " ");
+        System.out.print("]");
     }
     
     public static void printResultado (Rota [] vetor)
@@ -60,4 +65,34 @@ public class Print
         }
         System.out.println("SCV. Resultado: " + resultado);
     }
+    
+    public static void printResultado (Rota [][] matrizSCV)
+    {
+        if(matrizSCV != null) {
+            long resultadoPCV = 0;
+            for(int i = 0; i < matrizSCV.length; i++)
+                for(int j = 0; j < matrizSCV.length; j++)
+                    resultadoPCV += matrizSCV[i][j].getDistancia();
+            System.out.println("SCV. Tamanho: " + matrizSCV.length);
+            System.out.println("SCV. Resultado: " + resultadoPCV);
+        }
+        else
+            System.out.println("Matriz vazia");
+    }
+    
+    public static void printResultado (long [][] matriz, int [] scv)
+    {
+        long resultado = 0;
+        System.out.println("\n\nSCV. Tamanho: " + scv.length);
+        for(int i = 0; i < scv.length; i++)
+        {
+            
+            System.out.println( "("+scv[i]+")______" + 
+                                matriz[scv[i]][scv[i+1]] + "______(" + 
+                                scv[i+1]+ ")" );
+            resultado += matriz[scv[i]][scv[i+1]];
+        }
+        System.out.println("SCV. Resultado: " + resultado);
+    }
+    
 }
