d947068bfa0e8cf69a685f770092e859a0d90f42
Ednaldo <martinsednaldo@outlook.com>
2018-10-27 01:58:11 -0300

    Interface para refinar resultado da heurística.

---
+++ src/util/RefinamentoPCV.java
@@ -0,0 +1,13 @@
+
+package util;
+
+import model.Rota;
+
+/*******************************************************************************
+ * @author Ednaldo                                                             *
+ *  date: 26.10.2018                                                           *
+ ******************************************************************************/
+public interface RefinamentoPCV 
+{
+    void refinar(Rota r, long [][] grafo, int [] rota);
+}
