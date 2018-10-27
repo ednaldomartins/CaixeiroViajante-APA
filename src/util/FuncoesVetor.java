3c2238805a0983891d4add3db2ec1fc12ea50c00
Ednaldo <martinsednaldo@outlook.com>
2018-10-27 01:57:27 -0300

    Remoção de métodos desnecessários.

--- src/util/FuncoesVetor.java
+++ src/util/FuncoesVetor.java
@@ -1,7 +1,6 @@
 
 package util;
 
-import java.util.List;
 import model.Rota;
 
 /*******************************************************************************
@@ -17,45 +16,6 @@ public class FuncoesVetor {
         list[i] = temp;
     }
     
-    public static void copiarLista ( List <Rota> original, Rota [] copia )
-    {   //a posicao 0 guarda o numero de vertices
-        if(copia.length >= original.size()-1)
-        {
-            for(int i = 0; i < copia.length; i++)
-                copia[i] = original.get(i+1);
-        }
-        else
-            System.err.println("Nao foi possivel copiar o array original, pois o array orginal eh maior do que o vetor copia");
-    }
-    
-    //copiar apenas um lado da matriz para um novo vetor 
-    public static void copiarMatriz ( Rota[][] matriz, Rota [] vetor)
-    {
-        int k = 0;
-        for(int i = 0; i < matriz.length; i++)
-            for(int j = i+1; j < matriz.length; j++)
-                vetor[k++] =  matriz[i][j];
-    }
-    
-    //copiar apenas um lado da matriz para um novo vetor 
-    public static void copiarMatriz ( Rota[][] matriz, Rota [][] copia)
-    {
-        if(copia.length >= matriz.length)
-        {
-            int k = 0;
-            for(int i = 0; i < matriz.length; i++)
-                for(int j = 0; j < matriz.length; j++)
-                    copia[i][j] =  matriz[i][j];
-        }
-        else {System.err.println("Nao ha espaco suficiente na copia.");}
-    }
-    
-    public static void iniciarVetor(Rota[] vetor)
-    {
-        for(int i = 0; i < vetor.length; i++)
-            vetor[i] = new Rota( Long.MAX_VALUE, false );
-    }
-    
     public static void iniciarVetor(int[] vetor) 
     {
         for(int i = 0; i < vetor.length; i++)
@@ -68,28 +28,17 @@ public class FuncoesVetor {
             vetor[i] = 0;
     }
     
-    public static void iniciarMatriz(Rota[][] matriz)
-    {
-        for(int i = 0; i < matriz.length; i++)
-            for(int j = 0; j < matriz.length; j++)
-                matriz[i][j] =  new Rota( -1, -1, -1, 0, false );
-    }
-    
     public static void iniciarMatriz(long[][] matriz)
     {
         for(int i = 0; i < matriz.length; i++)
             for(int j = 0; j < matriz.length; j++)
                 matriz[i][j] = 0;
     }
-
-    public static int tamanhoLadoMatriz (Rota [][] matriz)
+    
+    public static void copiarVetor(int[] vetor, int [] copia) 
     {
-        int k = 0;
-        for(int i = 0; i < matriz.length; i++)
-            for(int j = i+1; j < matriz.length; j++)
-                if(matriz[i][j].getDistancia()!= 0)
-                    k++;
-        return k;
+        for(int i = 0; i < vetor.length; i++)
+            copia[i] = vetor[i];
     }
     
 }
