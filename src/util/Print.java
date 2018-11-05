5537bab6eab2b4319523d773d58c8f213a3d7421
Ednaldo <martinsednaldo@outlook.com>
2018-11-05 12:06:33 -0300

    Printando o vertice 0 como 1, e assim por diante.

--- src/util/Print.java
+++ src/util/Print.java
@@ -18,26 +18,26 @@ public class Print
         }
     }
     
-    public static void printVetor (int [] vetor)
+    public static void printVetor (int [] vetor, String nome)
     {
-        System.out.println("\n\nSCV. Tamanho: " + vetor.length + "\n[ ");
+        System.out.print("\n\n\n"+ nome +". Tamanho: " + vetor.length + "\n[ ");
         for(int i = 0; i < vetor.length; i++)
-            System.out.println( vetor[i] + " ");
+            System.out.print( (vetor[i]+1) + " ");
         System.out.print("]");
     }
     
-    public static void printResultado (long [][] matriz, int [] scv)
+    public static void printResultado (long [][] matriz, int [] scv, String nome)
     {
         long resultado = 0;
-        System.out.println("\n\nSCV. Tamanho: " + scv.length);
+        System.out.println("\n\n"+nome+". Tamanho: " + scv.length);
         for(int i = 0; i < scv.length-1; i++)
         {
-            System.out.println( "("+scv[i]+")______" + 
+            System.out.println( "("+(scv[i]+1)+")______" + 
                                 matriz[scv[i]][scv[i+1]] + "______(" + 
-                                scv[i+1]+ ")" );
+                                (scv[i]+2)+ ")" );
             resultado += matriz[scv[i]][scv[i+1]];
         }
-        System.out.println("SCV. Resultado: " + resultado);
+        System.out.println(nome+". Resultado: " + resultado);
     }
     
 }
